cae5eca3bd715ef27596d3c81cde390c90dabcc9
JakubMihalik <xmihalikj1@stuba.sk>
2023-03-13 14:26:59 +0100

    Atatatan2 v2

--- demoRMR/Controller.cpp
+++ demoRMR/Controller.cpp
@@ -34,6 +34,42 @@ Controller::ControllerOutput Controller::regulate()
     Controller::ErrorValue ev = Controller::calculateErrors();
     static Controller::ControllerOutput controllerOutput;
 
+    controllerOutput.reached = 0;
+    double radius = 0;
+
+    if (abs(ev.x) > 0.01 || abs(ev.y) > 0.01)
+    {
+        controllerOutput.forwardSpeed += 50 * sqrt(pow(ev.x, 2) + pow(ev.y, 2));
+        controllerOutput.forwardSpeed = min(controllerOutput.forwardSpeed, 400);
+    }
+    else
+    {
+        controllerOutput.reached = 1;
+
+        robot->setTranslationSpeed(0);
+
+        controllerOutput.forwardSpeed = 0;
+        controllerOutput.rotationSpeed = 0;
+        return controllerOutput;
+    }
+
+    if (abs(ev.theta) > deg2rad(0.01))
+    {
+        controllerOutput.rotationSpeed += ev.theta;
+//        controllerOutput.rotationSpeed = max(controllerOutput.rotationSpeed, -PI / 3.0);
+        radius = controllerOutput.forwardSpeed / controllerOutput.rotationSpeed;
+    }
+    else
+    {
+        radius = 32768;
+        controllerOutput.rotationSpeed = 0;
+    }
+
+    std::cout << "FWS = " << controllerOutput.forwardSpeed << std::endl;
+    std::cout << "RTS = " << controllerOutput.rotationSpeed << std::endl;
+
+    robot->setArcSpeed(controllerOutput.forwardSpeed, radius);
+
     return controllerOutput;
 }
 
