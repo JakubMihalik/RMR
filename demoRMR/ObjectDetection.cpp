8bf86bebea2008b2fec440124d15f349a949456c
JakubMihalik <xmihalikj1@stuba.sk>
2023-04-11 14:22:38 +0200

    Change lidar scan conditions

--- demoRMR/ObjectDetection.cpp
+++ demoRMR/ObjectDetection.cpp
@@ -44,7 +44,7 @@ void ObjectDetection::writeLidarMap(std::ofstream& file, OdometryData data, Lase
         file << x << "," << y << "\n";
 
         // Binary map
-        if (laser.Data[i].scanDistance < 3000.0 /*&& laser.Data[i].scanDistance > 130*/ && laser.Data[i].scanDistance > 640.0)
+        if ((laser.Data[i].scanDistance < 3000.0 && laser.Data[i].scanDistance > 130) && (laser.Data[i].scanDistance < 640.0 || laser.Data[i].scanDistance > 700.0))
         {
             int mapX = (MAP_SIZE / 2 - 1) + std::round(x / MAP_RESOLUTION);
             int mapY = (MAP_SIZE / 2 - 1) - std::round(y / MAP_RESOLUTION);
