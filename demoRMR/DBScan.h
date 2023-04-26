97934e0d31a35f1612b8f17d8641bdf463f1c2a3
Aleš Melichar <alesmelichar13@gmail.com>
2023-04-26 14:29:15 +0200

    and this

--- demoRMR/DBScan.h
+++ demoRMR/DBScan.h
@@ -3,6 +3,7 @@
 #define DBSCAN_H
 
 #include <vector>
+#include <queue>
 
 typedef struct {
     double x, y;
@@ -20,7 +21,7 @@ private:
     int minSamples;
 
     void expandCluster(std::vector<DBPoint>& points, DBPoint* point, const std::vector<int>& neighbors, int clusterId);
-    std::vector<int> regionQuery(const std::vector<DBPoint>& points, const DBPoint* point);
+    std::vector<int> findNeighbours(const std::vector<DBPoint>& points, const DBPoint* point);
 
     double distance(DBPoint p1, DBPoint p2) {
         return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
